07f2a642fa728919dc87417393d339e2d4aa4ef7
Kadali kousalya <99967333+kadalikousalya@users.noreply.github.com>
2025-12-28 21:11:46 +0530

    Time: 7 ms (42.49%) | Memory: 18.8 MB (43.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,19 +1,31 @@
 class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
+    def moveZeroes(self, arr: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i=0
-        j=1
-        while j<len(nums):
-            if nums[i]==0:
-                if nums[j]!=0:
-                    nums[i], nums[j] = nums[j], nums[i]
-                    i+=1
-                j+=1          
+        # i=0
+        # j=1
+        # while j<len(nums):
+        #     if nums[i]==0:
+        #         if nums[j]!=0:
+        #             nums[i], nums[j] = nums[j], nums[i]
+        #             i+=1
+        #         j+=1          
+        #     else:
+        #         i+=1
+        #         j+=1
+        # return nums
+        left,right=0,1
+        while right<len(arr):
+            if arr[left]==0:
+                if arr[right]!=0:
+                    arr[right],arr[left]=arr[left],arr[right]
+                    left+=1
+                right+=1
             else:
-                i+=1
-                j+=1
-        return nums
+                #arr[left],arr[right+1]=arr[right+1],arr[left]
+                left+=1
+                right+=1
+        return arr
             
         
